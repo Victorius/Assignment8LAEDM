0108cc2f8dc8a26b99dbd1bb2710c8129d43117c
Victorius <victorius@users.noreply.github.com>
2017-04-19 22:52:04 +0300

    Fixed bugs. Launched version

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import warnings
-
 import numpy as np
 from sklearn import metrics
 
 
 def calculate_similarity(array, similarity_type, used_users,users_d):
-    # result_matrix = [[0 for j in range(len(array))] for i in range(len(array))]
     if used_users is None:
         size = len(array)
         result_matrix = np.ones((size, size)) * 0
@@ -14,26 +11,26 @@ def calculate_similarity(array, similarity_type, used_users,users_d):
             for j in range(i, len(array)):
                 column_j = np.nan_to_num((array[j] - np.nanmean(array[j])))
                 # calculate the similarity by similarity_type.
-                # f = np.dot(column_i, column_j) / (np.sqrt(np.dot(column_j, column_j)) * np.sqrt(np.dot(column_i, column_i)))
-                value_of_sim = similarity_type(X=column_i, Y=column_j)
-                result_matrix[i][j] = value_of_sim[0][0]
-                result_matrix[j][i] = value_of_sim[0][0]
-        return None, np.array(result_matrix)
-    else:
-        size = len(used_users)
-        #user_dictionary = {}
-        result_matrix = np.ones((size, size)) * 0
-        for i in range(size):
-            #user_dictionary[used_users[i]] = i
-            column_i = np.nan_to_num((array[user_dict[used_users[i]]] - np.nanmean(array[user_dict[used_users[i]]])))
-            for j in range(i, size):
-                column_j = np.nan_to_num((array[user_dict[used_users[i]]] - np.nanmean(array[user_dict[used_users[j]]])))
-                # calculate the similarity by similarity_type.
-                # f = np.dot(column_i, column_j) / (np.sqrt(np.dot(column_j, column_j)) * np.sqrt(np.dot(column_i, column_i)))
                 value_of_sim = similarity_type(X=column_i, Y=column_j)
                 result_matrix[i][j] = value_of_sim[0][0]
                 result_matrix[j][i] = value_of_sim[0][0]
         return np.array(result_matrix)
+    else:
+        size = len(array[:,0])
+        user_dictionary_for_similarity = {}
+        result_matrix = np.ones((len(used_users), size)) * 0
+        for i in range(len(used_users)):
+            user_dictionary_for_similarity[used_users[i]] = i
+            array_list = array[user_dict[used_users[i]]]
+            column_i = np.nan_to_num( array_list - np.nanmean(array_list))#
+            for j in range(size):
+                array_list_j = array[j]
+                column_j = np.nan_to_num((array_list_j - np.nanmean(array_list_j)))#array[user_dict[used_users[j]]]
+                # calculate the similarity by similarity_type.
+                # f = np.dot(column_i, column_j) / (np.sqrt(np.dot(column_j, column_j)) * np.sqrt(np.dot(column_i, column_i)))
+                value_of_sim = similarity_type(X=column_i, Y=column_j)
+                result_matrix[i][j] = value_of_sim[0][0]
+        return user_dictionary_for_similarity,np.array(result_matrix)
 
 
 def prediction(sim_matrix, array_data, i_position, j_position):
@@ -48,38 +45,53 @@ def prediction(sim_matrix, array_data, i_position, j_position):
     return numenator / denominator
 
 
-def item_item_prediction(sim_matrix, array_data, i_position, j_position):
-    condition = [np.logical_not(np.isnan(array_data[:, j_position]))]
-    choice = [array_data[:, j_position]]
-    sd = np.select(condlist=condition, choicelist=choice)
-    sd = np.array([sd[i] - mean_items[i] if sd[i] != 0 else 0 for i in range(len(sd))])
+def item_item_prediction(sim_matrix, array_data, user_position, item_position, user_d, course_d):
+    selected_array_by_user = array_data[:, user_d[user_position]]
+    condition = [np.logical_not(np.isnan(selected_array_by_user))]
+    choice = [selected_array_by_user]
 
-    condition_sim = [sim_matrix[i_position] > 0, sim_matrix[i_position] == 1]
-    choice_sim = [sim_matrix[i_position], 0]
+    rating = np.select(condlist=condition, choicelist=choice)
+    rating = np.array([(rating[i] - mean_items[course_d[i+1]]) if rating[i] != 0 else 0 for i in range(len(rating))])
+
+    condition_sim = [sim_matrix[course_d[item_position]] > 0, sim_matrix[course_d[item_position]] == 1]
+    choice_sim = [sim_matrix[course_d[item_position]], 0]
     sd_sim = np.select(condition_sim, choice_sim)
-    numenator = np.dot(sd, sd_sim)
+
+    numenator = np.dot(rating, sd_sim)
     denominator = np.sum(sd_sim) - 1
-    result = mean_items[i_position] + numenator / denominator
+    result = mean_items[course_d[item_position]] + numenator / denominator
     return result
 
 
-def user_user_prediction(sim_matrix, array_data, user_position, item_position):
-    condition = [np.logical_not(np.isnan(array_data[:, user_position]))]
-    choice = [array_data[:, user_position]]
+def user_user_prediction(sim_matrix, array_data, user_position, item_position, user_dictionary,
+                         item_dictionary,similarity_dict):
+    users_selected = array_data[:, item_dictionary[item_position]]
+    condition = [np.logical_not(np.isnan(users_selected))]
+    choice = [users_selected]
     sd = np.select(condlist=condition, choicelist=choice)
-    sd = np.array([sd[i] - mean_users[i] if sd[i] != 0 else 0 for i in range(len(sd))])
+    asdf = []
+    for i in range(len(sd)):
+        if sd[i] != 0:
+            asdf.append(sd[i] - mean_users[user_dictionary[i+1]])
+        else:
+            asdf.append(0)
+    sd = np.array([sd[i] - mean_users[user_dictionary[i+1]] if sd[i] != 0 else 0 for i in range(len(sd))])
 
-    condition_sim = [sim_matrix[item_position] > 0, sim_matrix[item_position] == 1]
-    choice_sim = [sim_matrix[item_position], 0]
+    value_sim_selected = similarity_dict[user_position]
+    condition_sim = [sim_matrix[value_sim_selected] > 0,
+                     sim_matrix[value_sim_selected] == 1]
+    choice_sim = [sim_matrix[value_sim_selected], 0]
     sd_sim = np.select(condition_sim, choice_sim)
     numenator = np.dot(sd, sd_sim)
     denominator = np.sum(sd_sim) - 1
+    if np.count_nonzero(sd_sim) == 0:
+        denominator += 1
     result = mean_users[item_position] + numenator / denominator
     return result
 
 
 def loading_preparing_data():
-    data = np.genfromtxt('course.csv', delimiter=',')
+    data = np.genfromtxt('simple.csv', delimiter=',')
     data = data[1:].astype(np.int32)
     print(np.unique(data[:, 0]).size)
     print(np.unique(data[:, 1]).size)
@@ -103,33 +115,31 @@ def loading_preparing_data():
     return user_dict, course_dict, np.array(array_number), np.array(test_data)
 
 
-def calculate_rmse(training_dataset, test_dataset):
-    rmse_nominator_item_item = 0
-    rmse_nominator_user_user = 0
+def calculate_rmse(training_dataset, test_dataset, user_d, course_d, similarity_dictionary):
+    user_based_result = []
+    item_based_result = []
     for i in test_dataset:
         # calculation item-based CF and user-based CF.
         # calculation is going for selected item and user.
-        v1 = item_item_prediction(similarity_matrix_item_item, training_dataset, i[1].__int__() - 1, i[0].__int__() - 1)
-        v2 = user_user_prediction(similarity_matrix_user_user, training_dataset.transpose(), i[1].__int__() - 1,
-                                  i[0].__int__() - 1)
-        rmse_nominator_item_item += (v1 - i[2]) ** 2
-        rmse_nominator_user_user += (v2 - i[2]) ** 2
-    rmse_nominator_item_item = np.sqrt(rmse_nominator_item_item / len(test_dataset))
-    rmse_nominator_user_user = np.sqrt(rmse_nominator_user_user / len(test_dataset))
-    return rmse_nominator_item_item, rmse_nominator_user_user
+        v1 = item_item_prediction(similarity_matrix_item_item, training_dataset, i[0].__int__() , i[1].__int__(),
+                                  user_d, course_d)
+        v2 = user_user_prediction(similarity_matrix_user_user, training_dataset.transpose(), i[0].__int__() ,
+                                  i[1].__int__() ,   user_d, course_dictionary,similarity_dictionary)
+        user_based_result.append(v2)
+        item_based_result.append(v1)
+    rmse_users = metrics.mean_squared_error(np.nan_to_num(user_based_result), test_dataset[:,2])
+    rmse_items = metrics.mean_squared_error(np.nan_to_num(item_based_result), test_dataset[:, 2])
+    return rmse_users, rmse_items
 
 
 def calculate_mean_to_item(data,user_d, coure_d):
-    mean_value = {np.nanmean(i) for i in data}
+    mean_value = [np.nanmean(i) for i in data]
     return mean_value
 
 
 def calculate_mean_to_users(data, test_data, user_d, course_d):
-    mean_value = {}
     tr_data = data.transpose()
-    for row in test_data:
-        mean_value[user_d[row]] = np.nanmean(data[:, (user_d[row])])
-    # mean_value = [np.nanmean(i) for i in data.transpose()]
+    mean_value = [np.nanmean(i) for i in tr_data]
     return mean_value
 
 
@@ -142,10 +152,8 @@ mean_users = calculate_mean_to_users(train_data, np.unique(test_data[:, 0]), use
 # calculation similarity matricies for items and for users
 similarity_matrix_item_item = calculate_similarity(train_data, metrics.pairwise.cosine_similarity, None,None)
 
-similarity_matrix_user_user = calculate_similarity(train_data.transpose(), metrics.pairwise.cosine_similarity,
+user_dictionary_for_similarity,similarity_matrix_user_user = calculate_similarity(train_data.transpose(), metrics.pairwise.cosine_similarity,
                                                    np.unique(test_data[:, 0]),user_dict)
-
-rmse_u, rmse_i = calculate_rmse(train_data, test_data)
+rmse_u, rmse_i = calculate_rmse(train_data, test_data, user_dict,course_dictionary,user_dictionary_for_similarity)
 print(str(rmse_i) + " item-item")
 print(str(rmse_u) + " user-user")
-# prediction(similarity_matrix_item_item, train_data, 0, 4)
